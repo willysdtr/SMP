de2f8768ea26254906ac21ea0c3db091e0ab9955
0xE4F90A <dolphin.tougi.0527@gmail.com>
2025-06-12 21:59:08 +0900

    base script

---
+++ Assets/Scripts/_Tool/Test.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int m_ID = 0;
+
+    void Awake() // Application app;
+    {
+        
+    }
+
+    void Start() // -> app.Init();
+    {
+        
+    }
+
+    void Update() // -> app.Update();
+    {
+        
+    }
+}
