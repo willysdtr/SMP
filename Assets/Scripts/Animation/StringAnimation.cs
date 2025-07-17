8207a21b9f118117a051908eeb1edee35feba599
deneso1107 <sk1529rt@icloud.com>
2025-07-17 10:47:55 +0900

    とりあえず糸が上下左右に動き回るように 糸同士が重なるのでここはまた調整します。

--- Assets/Scripts/Animation/StringAnimation.cs
+++ Assets/Scripts/Animation/StringAnimation.cs
@@ -1,49 +1,52 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class StringAnimation : MonoBehaviour
 {
-    private InputSystem_Actions inputActions;
-    void Awake()
+    public void EndAnimarion()
     {
-        inputActions.Stirng.nami.performed += ctx =>
-        {
-            PlayAnimarion();
-        };
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+        Debug.Log("アニメーション終了しました");
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    void PlayAnimarion()
-    {
-        Animator animator = GetComponent<Animator>();
-        if (animator != null)
+        // 子オブジェクトを親から外してアクティブにする
+        foreach (Transform child in transform)
         {
-            animator.SetTrigger("Play"); // アニメーションを再生
+
+            // 先にワールド座標を保存しておく
+            Vector3 worldPos = child.position;
+            Quaternion worldRot = child.rotation;
+            Vector3 worldScale = child.lossyScale;//ワールド空間の絶対スケール
+
+            // 親子関係を解除
+            child.SetParent(null);
+
+            // ワールド位置と回転を再設定（Transformのズレ防止）
+            child.position = worldPos;
+            child.rotation = worldRot;
+            // スケールを再設定（特殊）
+            SetWorldScale(child, worldScale); // 
+
+            // 必要ならアクティブ化
+            child.gameObject.SetActive(true);
         }
     }
-    void OnEnable()
+    void SetWorldScale(Transform target, Vector3 desiredWorldScale)
     {
-        if (inputActions == null)
+        Transform parent = target.parent;
+        if (parent == null)
         {
-            inputActions = new InputSystem_Actions();
+            target.localScale = desiredWorldScale;
         }
-        inputActions.Enable();  
-
-    }
-    void OnDisable()
-    {
-        if (inputActions == null)
+        else
         {
-            inputActions = new InputSystem_Actions();
+            Vector3 parentScale = parent.lossyScale;
+            target.localScale = new Vector3//親のスケールを考慮してローカルスケールを計算
+                (
+                desiredWorldScale.x / parentScale.x,
+                desiredWorldScale.y / parentScale.y,
+                desiredWorldScale.z / parentScale.z
+            );
+            //desiredWorldScale = localScale × parentScale→localScale = desiredWorldScale ÷ parentScale
         }
-        inputActions.Stirng.Disable();
     }
 }
