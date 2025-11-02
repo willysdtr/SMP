262677f79b13b10dede527329dc6f3f51e6dded7
tanikun4 <tanikun410@gmail.com>
2025-11-02 23:24:07 +0900

    タイトル画面のボタン操作実装

--- Assets/Scripts/Title/TitleScene.cs
+++ Assets/Scripts/Title/TitleScene.cs
@@ -14,6 +14,15 @@ public class TitleScene : MonoBehaviour
             Debug.Log("タイトル画面からセレクト画面へ移動");
             SceneManager.LoadScene("SelectScene");
         };
+        inputActions.TItle.GameEnd.performed += ctx =>
+        {
+            Debug.Log("ゲーム終了");
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+            #else
+                Application.Quit();//ゲームプレイ終了
+            #endif
+        };
     }
     // Update is called once per frame
     void OnEnable()
